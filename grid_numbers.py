c05e7b773188b59d71cc774e876a42371b4b9500
peter.gemmell <peter.gemmell@mths.ca>
2022-06-05 21:15:59 +0000

    33 days

--- grid_numbers.py
+++ grid_numbers.py
@@ -7,61 +7,46 @@
 import random
 
 
-def grid_average(random, row, column):
-    # this function calculates the average of all the elements in a 2D array
+def average_of_numbers(passed_in_2D_list, rows, columns):
+    # this function finds the average of a 2D array
 
-    average = 0
-    for row_value in random:
-        for array_element in row_value:
-            average += array_element
+    total = 0
+    for row_value in passed_in_2D_list:
+        for single_element in row_value:
+            total += single_element
 
-    average = average / (row * column)
+    average = round(total / (rows * columns))
 
     return average
 
 
 def main():
-    # this function creates a 2D array
+    # this function uses a 2D array
 
-    grid = []
+    a_2d_list = []
 
     # input
-    rows = input("Enter in the number of rows: ")
-    columns = input("Enter in the number of columns: ")
-    print("")
+    rows_as_string = input("How many row would you like: ")
+    columns_as_string = input("How many columns would you like: ")
 
     try:
-        rows_int = int(rows)
-        columns_int = int(columns)
-
-        if rows_int < 0:
-            print("That is a invalid integer.")
-        elif columns_int < 0:
-            print("That is an invalid integer.")
-
-        else:
-            for loop_counter_rows in range(0, rows_int):
-                temp_column = []
-                for loop_counter_columns in range(0, columns_int):
-                    random_number = random.randint(0, 50)
-                    temp_column.append(random_number)
-                    print("{0} ".format(random_number), end="")
-                grid.append(temp_column)
-                print("")
-
-            rounded_grid_average = grid_average(grid, rows_int, columns_int)
+        rows = int(rows_as_string)
+        columns = int(columns_as_string)
+        for loop_counter_rows in range(0, rows):
+            temp_column = []
+            for loop_counter_columns in range(0, columns):
+                a_random_number = random.randint(0, 50)
+                temp_column.append(a_random_number)
+                print("{0} ".format(a_random_number), end="")
+            a_2d_list.append(temp_column)
             print("")
-            print(
-                "The average of the numbers in the  grid is: {0} ".format(
-                    rounded_grid_average
-                )
-            )
 
+        average = average_of_numbers(a_2d_list, rows, columns)
+        print("\nThe average of all the numbers is : {0} ".format(average))
     except Exception:
-        print("Invalid input, please try again.")
+        print("Invalid input entered, please try again.")
 
-    finally:
-        print("\nDone.")
+    print("\nDone.")
 
 
 if __name__ == "__main__":
